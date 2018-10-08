d2b1f3de0b178870ef2545f43b6a12bc75b73890
Edith Vasquez <edithmaday.vasquez@gmail.com>
2018-10-08 10:03:09 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,72 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 // Rutas
-
-// import { AppRoutingModule } from './app-routing.module';
 import { routing, AppRoutingProviders } from './app.routes';
-import { NavbarModule } from './shared/navbar/navbar.module';
-import { FooterModule } from './shared/footer/footer.module';
-import { SidebarModule } from './sidebar/sidebar/sidebar.module';
-
-// importar servicios
 
 
 
+// Componentes
 
-// importar componentes
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
-import { ErrorComponent } from './error/error.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-=======
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
->>>>>>> 163a3a88a4324f6f54cd8debb93e59d662a254ce
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LogComponent } from './log/log.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LoginComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
     ErrorComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-   //  AppRoutingModule,
-    RouterModule,
-    HttpModule,
-    routing,
-    NavbarModule,
-    FooterModule,
-    SidebarModule
+    routing
   ],
   providers: [
     AppRoutingProviders
   ],
-=======
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
->>>>>>> 163a3a88a4324f6f54cd8debb93e59d662a254ce
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,29 @@
 import {  ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
-
 // importar componentes
-import { LoginComponent } from './login/login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LogComponent } from './log/log.component';
+
+
 
 // Array  de rutas - configuracion
 
-const appROUTES: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'error', component: ErrorComponent },
-    { path: '* *',  pathMatch: 'full', redirectTo: 'error' }
+const ROUTES: Routes = [
+    { path: '  ', component: HomeComponent },
+     { path: 'home', component: HomeComponent },
+     { path: 'error', component: ErrorComponent },
+     { path: 'register', component: RegisterComponent },
+     { path: 'log', component: LogComponent },
+
+     { path: '* *',  component: HomeComponent},
 ];
-
-
 
 // exportar el modulo del router
 export const AppRoutingProviders: any [] = [];
-export const routing: ModuleWithProviders = RouterModule.forRoot(appROUTES);
+export const routing: ModuleWithProviders = RouterModule.forRoot(ROUTES);
+
+
 
