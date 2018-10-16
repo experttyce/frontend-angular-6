c50feb5ee5b33d2e6ac8072067b50901d9571783
Carlos Orozco <carlos.orozco@sekologistics.com>
2018-10-16 11:53:23 -0500

    conflicto en version de typescript

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  baseUrl: 'http://bk.sekoreports.com'
-};
+export const environment = { production: false, baseUrl: "http://127.0.0.1:8000" };
 
 /*
  * For easier debugging in development mode, you can import the following file
